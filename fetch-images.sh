567ea5813455d94a64c221a1cef24889cfc8c674
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2012-10-26 18:30:24 -0700

    Added fetch-images script for pulling in sample images

--- fetch-images.sh
+++ fetch-images.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# Set some variables
+IMAGE_ARCHIVE='http://github.com/downloads/UberGallery/scripts/sample-images.tar.gz'
+DL_LOCATION='/tmp/sample-images.tar.gz'
+IMAGE_DIR=$(pwd)/../gallery-images
+
+# Fetch the archive
+wget $IMAGE_ARCHIVE -O $DL_LOCATION
+
+# Extract the archive
+tar -xzvf $DL_LOCATION -C $IMAGE_DIR
