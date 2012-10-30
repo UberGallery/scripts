f5d1235ffa72d49272cc77468016bde051437c99
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2012-10-29 17:56:51 -0700

    Improvements to the image fetching script including checking directory existance and file permissions and improved script output

--- fetch-images.sh
+++ fetch-images.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
 # Set some variables
-IMAGE_ARCHIVE='http://github.com/downloads/UberGallery/scripts/sample-images.tar.gz'
-DL_LOCATION='/tmp/sample-images.tar.gz'
-IMAGE_DIR=$(pwd)/../gallery-images
+ARCHIVE_URL="http://github.com/downloads/UberGallery/scripts/sample-images.tar.gz"
+TEMP_DIR="/tmp"
+FILE_NAME="$TEMP_DIR/sample-images.tar.gz"
+IMAGE_DIR="$(dirname $(pwd))/gallery-images"
+
+# Verify /tmp directory permissions
+if [ ! -r $TEMP_DIR ] || [ ! -w $TEMP_DIR ]; then
+    echo "ERROR: $TEMP_DIR does not exist or you don't have permission to access it"
+    exit 1
+fi
+
+# Verify gallery image directory permissions
+if [ ! -r $IMAGE_DIR ] || [ ! -w $IMAGE_DIR ]; then
+    echo "ERROR: $IMAGE_DIR does not exist or you don't have permission to access it"
+    exit 1
+fi
 
 # Fetch the archive
-wget $IMAGE_ARCHIVE -O $DL_LOCATION
+echo "Downloading image archive from $ARCHIVE_URL"
+wget $ARCHIVE_URL --progress=dot:binary -O $FILE_NAME |& grep "%"
 
 # Extract the archive
-tar -xzvf $DL_LOCATION -C $IMAGE_DIR
+echo "Extracting images to $IMAGE_DIR"
+tar -xzvf $FILE_NAME -C $IMAGE_DIR
